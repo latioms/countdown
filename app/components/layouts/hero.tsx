41822a780c147ea355ae6cdd962b2a8338ffa06c
latioms <latioms@gmail.com>
2024-04-07 23:18:27 +0100

    Mettre à jour le composant Hero pour importer l'image de Next.js

--- app/components/layouts/hero.tsx
+++ app/components/layouts/hero.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import HeroForm from "../molecules/HeroForm";
+import Image from "next/image";
 
 function Hero() {
   return (
@@ -9,12 +10,12 @@ function Hero() {
           <h1 className="max-w-2xl mb-4 text-4xl font-bold tracking-tight leading-none md:text-5xl xl:text-5xl dark:text-white">
             Les meilleurs prestataires de services à portée de main
           </h1>
-          <p className="max-w-2xl mb-6 font-normal text-black lg:mb-6 md:text-lg lg:text-xl dark:text-gray-400">
+          <p className="max-w-2xl mb-6 font-light text-black lg:mb-6 md:text-lg lg:text-xl dark:text-gray-400">
             Hubblot est une plateforme qui met en relation les prestataires de services et les clients. 
             Vous permettant de trouver rapidement et facilement un prestataire de service dans votre ville.
           </p>
           <p className="max-w-xl mb-2 font-normal text-orange-800 lg:mb-2 md:text-lg lg:text-xl dark:text-gray-400">
-            L'application sera bientot disponible, mais vous pouvez nous decrire les services que vous recherchez.
+            L&apos;application sera bientot disponible, mais vous pouvez nous decrire les services que vous recherchez.
           </p>
           <HeroForm />  
 
